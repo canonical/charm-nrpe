ab947d33d267a524128a93131f601596c1cffb81
Paul Gear <paul.gear@canonical.com>
2016-11-02 11:59:01 +1000

    Add conntrack check RT#96968

---
+++ files/plugins/check_conntrack.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# This file is managed by juju.  Do not make local changes.
+
+# Copyright (C) 2013, 2016 Canonical Ltd.
+# Author: Haw Loeung <haw.loeung@canonical.com>
+#         Paul Gear <paul.gear@canonical.com>
+
+# Alert when current conntrack entries exceeds certain percentage of max. to
+# detect when we're about to fill it up and start dropping packets.
+
+set -eu
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+
+if ! lsmod | grep -q conntrack; then
+    echo "OK: no conntrack modules present"
+    exit $STATE_OK
+fi
+
+max=$(sysctl net.netfilter.nf_conntrack_max | awk '{ print $3 }')
+current=$(sysctl net.netfilter.nf_conntrack_count | awk '{ print $3 }')
+
+# default thresholds
+crit=90
+warn=80
+
+# parse command line
+set +e
+OPTIONS=$(getopt w:c: "$@")
+if [ $? -ne 0 ]; then
+    echo "Usage: $0 [-w warningpercent] [-c criticalpercent]" >&2
+    echo "       Check nf_conntrack_count against nf_conntrack_max" >&2
+    exit $STATE_UNKNOWN
+fi
+set -e
+
+set -- $OPTIONS
+while true; do
+    case "$1" in
+	-w) warn=$2; shift 2 ;;
+	-c) crit=$2; shift 2 ;;
+	--) shift;   break ;;
+	*)  break ;;
+    esac
+done
+
+percent=$((current * 100 / max))
+stats="| current=$current max=$max percent=$percent;$warn;$crit"
+
+threshold=$((max * crit / 100))
+if [ $current -gt $threshold ]; then
+    echo "CRITICAL: conntrack table nearly full. $stats"
+    exit $STATE_CRITICAL
+fi
+
+threshold=$((max * warn / 100))
+if [ $current -gt $threshold ]; then
+    echo "WARNING: conntrack table filling. $stats"
+    exit $STATE_WARNING
+fi
+
+echo "OK: conntrack table normal $stats"
+exit $STATE_OK
