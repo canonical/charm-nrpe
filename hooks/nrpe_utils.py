553278fd4bf014f7663e2454baf403639ce47236
Tom Haddon <tom@haddons.net>
2017-09-27 09:50:57 +0100

    Remove nrpe.d files each time before rendering them to ensure we have a clean directory lp#1629127

--- hooks/nrpe_utils.py
+++ hooks/nrpe_utils.py
@@ -46,6 +46,12 @@ def remove_host_export_fragments(service_name):
         os.unlink(fname)
 
 
+def remove_nrped_files():
+    """ Remove nrpe.d files """
+    for fname in glob.glob('/etc/nagios/nrpe.d/*.cfg'):
+        os.unlink(fname)
+
+
 def install_charm_files(service_name):
     """ Install files shipped with charm """
     nag_dirs = ['/etc/nagios/nrpe.d/', '/usr/local/lib/nagios/plugins',
@@ -91,6 +97,8 @@ def render_nrpe_check_config(checkctxt):
 
 def render_nrped_files(service_name):
     """ Render each of the predefined checks """
+    # Remove existing first in case sub_postfix has changed, for instance
+    remove_nrped_files()
     for checkctxt in nrpe_helpers.SubordinateCheckDefinitions()['checks']:
         render_nrpe_check_config(checkctxt)
     process_local_monitors()
