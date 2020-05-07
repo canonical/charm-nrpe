6a41da382a83a6aa914201712b82fa4cb62bc7eb
Yoshi Kadokawa <yoshi.kadokawa@canonical.com>
2020-05-07 17:53:09 +0900

    add details of lacpdu port mismatch
    LP: #1877255

--- files/plugins/check_lacp_bond.py
+++ files/plugins/check_lacp_bond.py
@@ -17,6 +17,30 @@ from nagios_plugin3 import (
     try_check,
 )
 
+# LACPDU port states in binary
+LACPDU_ACTIVE = 0b1
+LACPDU_RATE = 0b10
+LACPDU_AGGREGATED = 0b100
+LACPDU_SYNC = 0b1000
+LACPDU_COLLECT = 0b10000
+LACPDU_DIST = 0b100000
+LACPDU_DEFAULT = 0b1000000
+LACPDU_EXPIRED = 0b10000000
+
+
+def check_lacpdu_port(actor_port, partner_port):
+    diff = int(actor_port) ^ int(partner_port)
+    msg = ''
+    if diff & LACPDU_RATE:
+        msg += 'lacp rate mismatch,'
+    if diff & LACPDU_AGGREGATED:
+        msg += 'not aggregated,'
+    if diff & LACPDU_SYNC:
+        msg += 'not in sync,'
+    if diff & LACPDU_COLLECT:
+        msg += 'not collecting,'
+    return msg
+
 
 def check_lacp_bond(iface):
     """Checks LACP bonds are correctly configured (AD Aggregator IDs match)
@@ -25,8 +49,12 @@ def check_lacp_bond(iface):
     BOND_SLAVES_TEMPLATE = '/sys/class/net/{0}/bonding/slaves'
     BOND_MODE_TEMPLATE = '/sys/class/net/{0}/bonding/mode'
     SLAVE_TEMPLATE = '/sys/class/net/{0}/bonding_slave/ad_aggregator_id'
-    ACTOR_PORT_STATE = '/sys/class/net/{0}/bonding_slave/ad_actor_oper_port_state'
-    PARTNET_PORT_STATE = '/sys/class/net/{0}/bonding_slave/ad_partner_oper_port_state'
+    ACTOR_PORT_STATE = (
+        '/sys/class/net/{0}/bonding_slave/ad_actor_oper_port_state'
+    )
+    PARTNET_PORT_STATE = (
+        '/sys/class/net/{0}/bonding_slave/ad_partner_oper_port_state'
+    )
 
     bond_aggr = BOND_AGGR_TEMPLATE.format(iface)
     bond_slaves = BOND_SLAVES_TEMPLATE.format(iface)
@@ -46,7 +74,7 @@ def check_lacp_bond(iface):
 
         with open(bond_slaves) as fd:
             slaves = fd.readline().strip().split(' ')
-
+        # Check aggregator ID
         for slave in slaves:
             with open(SLAVE_TEMPLATE.format(slave)) as fd:
                 slave_aggr_value = fd.readline().strip()
@@ -60,17 +88,18 @@ def check_lacp_bond(iface):
                 msg = msg.format(iface, bond_aggr_value,
                                  slave, slave_aggr_value)
                 raise WarnError(msg)
-
+        # Check LACPDU port state
+        for slave in slaves:
             with open(ACTOR_PORT_STATE.format(slave)) as fd:
                 actor_port_value = fd.readline().strip()
-
             with open(PARTNET_PORT_STATE.format(slave)) as fd:
                 partner_port_value = fd.readline().strip()
-            
             if actor_port_value != partner_port_value:
-                msg = 'WARNING: LACPDU port state mismatch'
-                msg += '({0} actor port state:{1} - {0} partner port state:{2})'
-                msg = msg.format(slave, actor_port_value,
+                res = check_lacpdu_port(actor_port_value, partner_port_value)
+                msg = 'WARNING: LACPDU port state mismatch '
+                msg += '({0}: {1} - actor_port_state={2}, '
+                msg += 'partner_port_state={3})'
+                msg = msg.format(res, slave, actor_port_value,
                                  partner_port_value)
                 raise WarnError(msg)
 
@@ -104,6 +133,7 @@ def main():
     args = parse_args()
     try_check(check_lacp_bond,
               args.iface)
+
 
 if __name__ == '__main__':
     main()
